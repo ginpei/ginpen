f48114fe1453cbbb9791ade179ee498a7bd2c21d
ginpei <ginpei@gmail.com>
2020-11-07 15:09:51 -0800

    remove unneccessary code in TS

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -78,17 +78,4 @@ const SEO: React.FC<{
   );
 };
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-};
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-};
-
 export default SEO;
